72512b2ac682469762362a8b67c8f21f1334672f
lukeathedev <alvarengalucas55@gmail.com>
2021-05-31 10:58:57 -0300

    Update Manifest

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,15 @@ export default class Header extends React.Component {
   render() {
     return (
       <div className="Header ac1">
-        <h1>Lucas' Portfolio<br></br><i className="fg1">Last updated {new Date(this.props.date).toLocaleDateString()}.</i></h1>
+        <div>
+          {/* <h3 className="blogLink">
+            <a href="#" className="fg0">Home</a> | <a href="#" className="fg1">Blog</a></h3>
+          <br></br> */}
+          <h1>
+            Lucas' Portfolio<br></br>
+            <i className="fg1">Last updated {new Date(this.props.date).toLocaleDateString()}.</i>
+          </h1>
+        </div>
         <button onClick={this.props.themeFunc}>{this.props.theme} Mode</button>
       </div>
     );
